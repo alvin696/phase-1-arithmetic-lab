fc31df39528832c8b66eac6aa82363d5bab12b20
Alvin kipkirui yaem <alvinyaem4@gmail.com>
2023-03-18 10:40:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+//Challenge 1
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
 
-// Write your code here
+//Challenge 2
+let random = Math.floor(Math.random() * 100) + 1
+
+//Challenge 3
+let num3 = 16
+let num4 = 6
+let mod = num3 % num4
+
+//Challenge 4
+let set = [2, 5, 7, 8, 20]
+let max = Math.max(...set)
